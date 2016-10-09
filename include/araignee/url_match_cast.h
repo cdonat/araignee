b71d3d54dec9bb9f48ae4bb7bee13de5a80f73d3
Christof Donat <cd@okunah.de>
2016-10-09 12:02:32 +0200

    extract repettitive code in common base class

--- include/araignee/url_match_cast.h
+++ include/araignee/url_match_cast.h
@@ -9,26 +9,29 @@
 
 namespace araingee
 {
-    template<typename T>
-    class url_match_cast {
+    class url_match_cast_base {
+        protected:
             std::string format_;
         public:
-            url_match_cast(const std::string& format) : format_{format} {};
-            url_match_cast(std::string&& format) : format_{std::move(format)} {};
-            url_match_cast(const url_match_cast& o) : format_{o.format_} {};
-            url_match_cast(url_match_cast&& o) : format_{std::move(o.format_)} {};
-            T operator () (const std::smatch& m) { return boost::lexical_cast<T>(m.format(format_)); };
+            url_match_cast_base(const std::string& format) : format_{format} {};
+            url_match_cast_base(std::string&& format) : format_{std::move(format)} {};
+            url_match_cast_base(const char* format) : format_{format} {};
+            url_match_cast_base(const url_match_cast_base& o) : format_{o.format_} {};
+            url_match_cast_base(url_match_cast_base&& o) : format_{std::move(o.format_)} {};
+    };
+
+    template<typename T>
+    struct url_match_cast : public url_match_cast_base {
+        template <typename P>
+        url_match_cast(P&& p): url_match_cast_base{std::forward<P>(p)} {};
+        T operator () (const std::smatch& m) { return boost::lexical_cast<T>(m.format(format_)); };
     };
 
     template <>
-    class url_match_cast<std::string> {
-            std::string format_;
-        public:
-            url_match_cast(const std::string& format) : format_{format} {};
-            url_match_cast(std::string&& format) : format_{std::move(format)} {};
-            url_match_cast(const url_match_cast& o) : format_{o.format_} {};
-            url_match_cast(url_match_cast&& o) : format_{std::move(o.format_)} {};
-            std::string operator () (const std::smatch& m) { return m.format(format_); };
+    struct url_match_cast<std::string> : public url_match_cast_base {
+        template <typename P>
+        url_match_cast(P&& p): url_match_cast_base{std::forward<P>(p)} {};
+        std::string operator () (const std::smatch& m) { return m.format(format_); };
     };
 }
 
